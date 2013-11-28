ab0053d002392795cbf0f38d85d9018341b1e0e5
sdopoku <dopoku1@adoore.org>
2013-11-28 15:39:23 -0500

    Update by sdopoku for create_songfeatures_file

--- DataFormat.py
+++ DataFormat.py
@@ -7,59 +7,82 @@ for user by the similarity classes
 """
 
 from subprocess import check_output
-import hdf5_getters as HDF5
+import hdf5_getters as hdf5
+import os
+import pandas as pd
 
-    
-"""
--Creates a user dictionary of songs and play counts and a list of songs from the 
-Echo Nest Taste Profile dataset
--Input: Echo Nest Taste Profile dataset
--Output: 1)Nested dictionary --> {userID:{songID:playCount},...}
-         2)List of unique SongID in Taste Profile dataset
-"""
-def parse_TasteProfile(tasteProfile):
-    TP = open(tasteProfile, 'r')
-    userDict = {} #Nested dictionary to store userID and songID:playCount mapping
-    songDict = {} #Dictionary to store unique songIDs in Echo Nest Taste Profile dataset
+
+def parse_taste_profile(taste_profile):
+    """
+    -Creates a user dictionary of songs and play counts and a list of songs from the 
+    Echo Nest Taste Profile dataset
+    -Input: Echo Nest Taste Profile dataset
+    -Output: 1)Nested dictionary --> {userID:{songID:playCount},...}
+             2)List of unique SongID in Taste Profile dataset
+    """
+
+    tp = open(taste_profile, 'r')
+    user_dict = {} #Nested dictionary to store userID and songID:playCount mapping
+    song_dict = {} #Dictionary to store unique songIDs in Echo Nest Taste Profile dataset
     
     #create nested dictionary of userID to songID,playcount
-    for taste in TP:
-        userID, songID,playCount = taste.split()
-        playCount = int(playCount)
+    for taste in tp:
+        user_id, song_id,play_count = taste.split()
+        play_count = int(play_count)
 
-        if( not userDict.has_key(userID)): #if userID not key of userDict 
-            userDict[userID] = {songID:playCount} #create new userID key
+        if( not user_dict.has_key(user_id)): #if user_id not key of user_dict 
+            user_dict[user_id] = {song_id:play_count} #create new userID key
         else:
-            userDict[useIDr][songID] =playCount
+            user_dict[user_id][song_id] =play_count
             
-        if(not songDict.has_key(songID)): 
-            songDict[songID]=0 #Store a dummy value
+        if(not song_dict.has_key(song_id)): 
+            song_dict[song_id]=0 #Store a dummy value
 
-    songList = songDict.keys()  #Save unique song ID into list
-    return userDict, songList
+    #song_list = song_dict.keys()  #Save unique song_id into list
+    tp.close()
+    return user_dict, song_dict
 
 
-#def rename_HDF5():
-
-"""
--Creates a dictionary of a songID to features provided by the Million Song Dataset(MSD)
--Input: 1) path to root folder storing HDF5 MSD files: Ex './MillionSongSubset/data/'
-        2) List of unique songIDs from Echo Nest Taste Profile dataset
--Output: A dictionary of songID to MSD feature mapping ---yet to decide on what features to 
-         select 
-"""
-        
-def create_song_dict(rootDir,songList):
-    for songID in songList:
-        songFilename = songID+".h5" #add .h5 extension to end of songID
-        try:
-            songPath = check_output("find rootDir -type f -name songFilename", shell=True).strip()
-            songObject = HDF5.open_h5_file_read(songPath) #create HDF5 object
-            songYear = HDF5.get_year(songObject)
-            print "%s : %s" %(songID,songYear) #This is just a test print statement
-        except: 
-            pass #ignore system exit error from check_output command
+def get_track_filenames(root_dir):#,dest_filename):
+    #mssf=open(dest_filename,'w')
+    mssf_test = []
     
+    for root, dirs, files in os.walk(root_dir,topdown=False):
+        for name in files:
+            filename = root+"/"+name
+            #mssf.write(filename)
+            mssf_test.append(filename)
+    return list(set(mssf_test))
+
+
+
+def create_songfeature_file(trhdf5,song_dict,output_file):
+    song_features = open(output_file,'w')
+    header_str = "sid, samprt, danc, dur, end_fad_in,energy, bars_conf, bars_st, beats_conf, beats_st, sec_conf, sec_st, seg_conf, seg_ld_max, seg_ld_maxtm, seg_ld_st, seg_pch, seg_st, seg_tmb, tat_conf, tat_st, key, key_conf, ld, mode, mode_conf, st_fd_out, tempo, tm_sig, tm_sig_conf, art_fam, art_hot, song_hot\n"  
+    song_features.write(header_str)
+
+    for hdf5_path in trhdf5:
+        try:
+            hdf5_file = pd.HDFStore(hdf5_path)
+            
+            song_id = hdf5_file["/metadata/songs"].values[0][-3]
+            if song_dict.has_key(song_id):
+                analysis = hdf5_file["/analysis/songs"].values[0]
+                analist = list(analysis[2:30])
+                analist.insert(0,analysis[0])
+                metadata = hdf5_file["/metadata/songs"].values[0]
+                metalist = [metadata[2],metadata[3], metadata[-4]]
+                analist.extend(metalist)
+                analist.insert(0,song_id)
+                feature_str = str(analist).strip('[]') +"\n"
+                song_features.write(feature_str)
+            
+
+        except:
+            print "Cannot open file: %s" %(hdf5_path)
+            pass
+        hdf5_file.close()
+    song_features.close()
         
     
 
